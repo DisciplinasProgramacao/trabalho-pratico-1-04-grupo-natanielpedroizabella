5275c58548c6100dacb91a17853d5652cd81653b
Nataniel93 <nataniel.geraldo@yahoo.com.br>
2022-04-28 18:48:48 -0300

    Criacao classe ItemMochila

---
+++ Código/src/App.java
@@ -0,0 +1,6 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        ItemMochila teste = new ItemMochila(10, 15);
+        System.out.println(teste.toString());
+    }
+}
