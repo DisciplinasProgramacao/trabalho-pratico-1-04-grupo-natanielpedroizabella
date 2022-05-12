df1034edb115bfe5b31be609f0012ce87947c479
Nataniel93 <nataniel.geraldo@yahoo.com.br>
2022-05-11 23:41:54 -0300

    Merge conflict final

--- Código/App.java
+++ Código/App.java
@@ -52,52 +52,6 @@ public class App {
 
         public static void main(String[] args) throws Exception {
 
-<<<<<<< HEAD
-            pesoAdicional = (pesoControle - peso > capacidade / 2) ? random.nextInt(capacidade / 2)
-                    : 1 + random.nextInt(pesoControle - peso);
-            itens[index % numeroDeItens].addPeso(pesoAdicional);
-            peso += pesoAdicional;
-            index++;
-
-        }
-        ;
-
-        return itens;
-    }
-
-    public static void main(String[] args) throws Exception {
-
-        long tempoInicial;
-        long tempoFinal;
-        long tempoExecucao;
-        int capacidade = 50;
-        ItemMochila[] itemMochila;
-
-        List<List<ItemMochila>> listaForcaBruta = new ArrayList<>();
-
-        // Geração itens Mochila
-        for (int j = 0; j < 500; j++) {
-
-            itemMochila = geradorDeItens(capacidade, 5);
-
-            // Combinacoes de 1 até N+1 elementos
-            tempoInicial = System.currentTimeMillis();
-            ForcaBruta forcaBruta = new ForcaBruta();
-
-            for (int i = 1; (i < itemMochila.length + 1); i++) {
-                ItemMochila data[] = new ItemMochila[itemMochila.length + 1];
-                forcaBruta.forcaBruta(itemMochila, data, 0, itemMochila.length - 1, 0,
-                        i, capacidade);
-            }
-            tempoFinal = System.currentTimeMillis();
-            tempoExecucao = tempoFinal - tempoInicial;
-
-            listaForcaBruta.add(forcaBruta.getLista());
-        }
-
-        System.out.println("Fim");
-    }
-=======
                 long tempoInicial;
                 long tempoFinal;
                 long tempoExecucao;
@@ -136,5 +90,4 @@ public class App {
 
                 System.out.println("Fim");
         }
->>>>>>> branchFinal
 }
