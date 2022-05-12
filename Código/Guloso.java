998a6eeb9fcf1b5b8c7d31887c563cd422c221b6
Nataniel93 <nataniel.geraldo@yahoo.com.br>
2022-05-11 22:44:50 -0300

    Commit final

--- Código/Guloso.java
+++ Código/Guloso.java
@@ -1,5 +1,8 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Guloso {
-    
+
     private ItemMochila ordenado[];
     private int quantidade;
     private int capacidade;
@@ -8,51 +11,41 @@ public class Guloso {
         return ordenado;
     }
 
-
     public void setOrdenado(ItemMochila[] ordenado) {
         this.ordenado = ordenado;
     }
-
 
     public int getQuantidade() {
         return quantidade;
     }
 
-
     public void setQuantidade(int quantidade) {
         this.quantidade = quantidade;
     }
-
 
     public int getCapacidade() {
         return capacidade;
     }
 
-
     public void setCapacidade(int capacidade) {
         this.capacidade = capacidade;
     }
 
-
-    public Guloso(ItemMochila ordenadoVp[], int qtd, int capacidade){
+    public Guloso(ItemMochila ordenadoVp[], int qtd, int capacidade) {
         this.ordenado = new ItemMochila[qtd];
         this.ordenado = ordenadoVp;
         this.quantidade = qtd;
         this.capacidade = capacidade;
     }
 
-
-    public ItemMochila[] mochilaGulosa (){
-        ItemMochila resposta[] = new ItemMochila[quantidade];
-        int i = 0;
+    public List<ItemMochila> mochilaGulosa() {
+        List<ItemMochila> resposta = new ArrayList<>();
         for (ItemMochila itemMochila : this.ordenado) {
-            if(itemMochila.getPeso() <= this.capacidade){
-                resposta[i] = itemMochila;
-                i++;
+            if (itemMochila.getPeso() <= this.capacidade) {
+                resposta.add(itemMochila);
                 this.capacidade -= itemMochila.getPeso();
             }
         }
-
         return resposta;
     }
 
