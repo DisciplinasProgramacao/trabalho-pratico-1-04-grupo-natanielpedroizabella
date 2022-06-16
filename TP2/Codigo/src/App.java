2907847ca368849075e5a0215d27bd9f92d1ee03
Pedro Miranda <pedromiranda@Pedros-MacBook-Air.local>
2022-06-16 10:39:20 -0300

    organinar classes

--- TP2/Codigo/src/App.java
+++ TP2/Codigo/src/App.java
@@ -1,9 +1,4 @@
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
-import java.util.Scanner;
 
 import model.Rolo;
 import services.ReadFile;
