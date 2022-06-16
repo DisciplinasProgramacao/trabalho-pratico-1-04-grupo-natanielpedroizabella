8d726d3cdcffc0bd60d4b96971821bd6f5520fc5
Pedro Miranda <pedromiranda@Pedros-MacBook-Air.local>
2022-06-16 10:32:19 -0300

    organinar classes

--- TP2/Codigo/src/App.java
+++ TP2/Codigo/src/App.java
@@ -6,29 +6,15 @@ import java.util.List;
 import java.util.Scanner;
 
 import model.Rolo;
+import services.ReadFile;
 
 
 public class App {
 
-    public static List<Rolo> readFile(String fileName)
-            throws FileNotFoundException, NullPointerException {
-
-        List<Rolo> rolers = new ArrayList<>();
-        Scanner sc = new Scanner(new File(fileName));
-
-        while (sc.hasNextLine()) {
-            String str = sc.nextLine();
-            int[] ctrl = Arrays.stream(str.split(" "))
-                    .mapToInt(s -> s.equals("x") ? -1 : Integer.parseInt(s)).toArray();
-            rolers.add(new Rolo(ctrl[0], ctrl[1], ctrl[2], ctrl[3]));
-        }
-
-        sc.close();
-        return rolers;
-    }
+    
 
     public static void main(String[] args) throws Exception {
-        List<Rolo> rolers = readFile("src/files/LaminacaoTeste1.txt");
+        List<Rolo> rolos = ReadFile.listarRolos("src/files/LaminacaoTeste1.txt");
 
     }
 
