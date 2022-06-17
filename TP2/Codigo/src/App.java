a539336c02945cf14264b4b25bbfb1f992b9cf92
Nataniel93 <nataniel.geraldo@yahoo.com.br>
2022-06-16 21:53:34 -0300

    Ajustes gerais

--- TP2/Codigo/src/App.java
+++ TP2/Codigo/src/App.java
@@ -1,6 +1,4 @@
 import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileWriter;
 import java.util.List;
 import java.util.Stack;
 
@@ -20,22 +18,28 @@ public class App {
                 "TP2/Codigo/src/files/LaminacaoTeste3.txt",
                 "TP2/Codigo/src/files/LaminacaoTeste4.txt"
         };
+
         BufferedWriter bw = GerenciarArquivo.abrirRelatorio();
+
         for (int i = 0; i < arquivos.length; i++) {
+
             List<Rolo> rolos = GerenciarArquivo.listarRolos(arquivos[i]);
             int espInicial = ListaRolo.maiorEspessura(rolos);
 
-            System.out.println("\n**** Laminação Teste " + (i + 1) + " ****\n");
-            System.out.println("**** Resultados bactracking ****\n");
+            System.out.println("\n\n--------Laminação Teste " + (i + 1) + "--------\n");
+
+            System.out.println("**** Resultados backtracking ****\n");
             BackTracking bt = new BackTracking();
             bt.backTracking(rolos, espInicial, new Stack<Rolo>(), 0);
             System.out.println("Custo: " + bt.getMenorCusto());
             System.out.println("Rolos usados: " + bt.getRolosSolucao());
+
             System.out.println("\n**** Resultados guloso ****\n");
             Guloso guloso = new Guloso();
             guloso.sequenciaRolos(rolos, espInicial);
             System.out.println("Custo: " + guloso.getMenorCusto());
             System.out.println("Rolos usados: " + guloso.getSequenciaRolos());
+
             System.out.println("\n**** Resultados progamação dinâmica ****\n");
             Dinamica pd = new Dinamica(rolos);
             pd.progamacaoDinamica(rolos);
